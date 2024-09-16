cae6e3c9fffd2f393d10eea5a787023b5e0fa9e6
Charlie Schlosser <cs.schlosser@gmail.com>
2024-09-15 23:54:08 -0400

    make all ctors inline constexpr

--- Eigen/src/Core/PlainObjectBase.h
+++ Eigen/src/Core/PlainObjectBase.h
@@ -485,7 +485,7 @@ class PlainObjectBase : public internal::dense_xpr_base<Derived>::type
   }
 #endif
 
-  EIGEN_DEVICE_FUNC constexpr PlainObjectBase(PlainObjectBase&& other) = default;
+  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE constexpr PlainObjectBase(PlainObjectBase&& other) = default;
 
   EIGEN_DEVICE_FUNC constexpr PlainObjectBase& operator=(PlainObjectBase&& other) EIGEN_NOEXCEPT {
     m_storage = std::move(other.m_storage);
